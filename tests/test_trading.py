76a91a65dcbbbe9e1327abc987ce84998bc1845c
Sidd <sidd.s.pai@gmail.com>
2024-12-03 12:24:38 -0700

    Updated tests.

--- tests/test_trading.py
+++ tests/test_trading.py
@@ -1,9 +1,11 @@
 import pytest
 
-from src.main import Account
+from src.main import Account, STOCK_PRICES, Action, Order, Status
 
 
 def test_account_creation() -> None:
     with pytest.raises(ValueError):
         acc = Account(-1)
+    with pytest.raises(TypeError):
+        acc = Account("abab")  # type: ignore
     acc = Account(10_000.0)
